9099b7e384c1ebd3e6fd0803d6700546ba91a9ab
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2025-03-23 15:07:37 +0200

    Added new files, updated existing ones

---
+++ musician/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Musician
+from .serializers import MusicianSerializer
+
+
+class MusicianViewSet(viewsets.ModelViewSet):
+    queryset = Musician.objects.all()
+    serializer_class = MusicianSerializer
+
+
+class MusicianViewSet(viewsets.ModelViewSet):
+    queryset = Musician.objects.all()
+    serializer_class = MusicianSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["instrument", "age"]
+    search_fields = ["first_name", "last_name", "instrument"]
+    ordering_fields = ["age", "date_of_applying"]
