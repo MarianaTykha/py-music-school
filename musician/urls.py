4a95248563b1d4dd4ef25fe126c8b97aad955332
mariana-tykha <marianatikhaya@gmail.com>
2025-03-28 13:36:10 +0200

    Fixed merge conflicts and updated files

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,12 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from musician.views import MusicianViewSet
 
 app_name = "musician"
+
+router = DefaultRouter()
+router.register("manage", MusicianViewSet, basename="manage")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
